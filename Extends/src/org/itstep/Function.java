5d76cfa5a9d18f045e9a10fdda34242ef55350af
hp <hp@172.31.3.2>
2018-06-07 19:40:28 +0300

    Commit

---
+++ Extends/src/org/itstep/Function.java
@@ -0,0 +1,15 @@
+package org.itstep;
+
+public class Function extends Number{
+	int x = 10;
+	int y = 20;
+	
+	public int getFunction() {
+		
+		int sum = minus(x, y);
+		
+		return sum;
+	}
+	
+	
+}
---
+++ Extends/src/org/itstep/Number.java
@@ -0,0 +1,17 @@
+package org.itstep;
+
+public class Number {
+
+	public int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int minus(int num1, int num2) {
+		return num1 - num2;
+	}
+	
+	public int umn(int num1, int num2) {
+		return num1*num2;
+	}
+
+}
